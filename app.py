e12af3b40d6c5265925855146735146f80adfc59
Saif Ullah <saifu0905@gmail.com>
2026-01-14 16:17:01 +0500

    Added Logging in the app.py via the logger.py in the utils folder

--- app.py
+++ app.py
@@ -6,5 +6,18 @@ from datetime import datetime
 from pathlib import Pathlib
 from streamlit_option_menu import option_menu
 from agents.orchestrator import OrchestratorAgent
+from utils.logger import setup_logger
+from utils.exception import ResumeProcessingError
+
+# Configure Streamlit page
+st.set_page_config(
+    page_title = "AI Recruiter Agency",
+    page_icon = "🤖",
+    layout = "wide"
+    initial_sidebar_state = "expanded"
+)
+
+# Initialize logger
+logger = setup_logger()
 
 
---
+++ utils/logger.py
@@ -0,0 +1,22 @@
+import logging
+from datetime import datetime
+import os
+
+def setup_logger():
+    """Setup applicaiton logging"""
+    logs_dir = "logs"
+    if not os.path.exists(logs_dir):
+        os.makedirs(logs_dir)
+
+    log_file = os.path.join(
+        logs_dir, f"recruitment_{datetime.now().strftimef('%Y%m%d_%H%M%S')}.log"
+
+    loggin.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            handlers=[
+                logging.FileHandler(log_file),
+                logging.StreamHandler() 
+            ]
+        )
+    return logging.getLogger("AI_Recruiter")
